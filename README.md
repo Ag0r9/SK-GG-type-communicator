39d3e5bd5ded2ace5c025ada2704275d18b93ff0
Ag0r9 <51130332+Ag0r9@users.noreply.github.com>
2022-01-10 20:27:20 +0100

    first ui added

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 # SK-GG-type-communicator
+## How to run a server?
+Run `run.sh`
+## PyQt important commands
+### How to open qt designer?
+`qt5-tools designer`
+### How to export .ui to .py?
+`pyuic5 -x <input_file> -o <output_file>`
---
+++ main.py
@@ -0,0 +1,160 @@
+import sys
+import socket
+from threading import Thread
+
+from PyQt5.uic import loadUi
+from PyQt5 import QtWidgets
+from PyQt5.QtWidgets import QDialog, QApplication
+
+
+class WelcomePage(QDialog):
+    def __init__(self):
+        super(WelcomePage, self).__init__()
+        loadUi('UI/welcome_page.ui', self)
+        self.login.clicked.connect(self.gotologin)
+        self.signup.clicked.connect(self.gotoregistration)
+
+    def gotologin(self):
+        login = LoginPage()
+        widget.addWidget(login)
+        widget.setCurrentIndex(widget.currentIndex()+1)
+
+    def gotoregistration(self):
+        registration = RegistrationPage()
+        widget.addWidget(registration)
+        widget.setCurrentIndex(widget.currentIndex()+1)
+
+
+class LoginPage(QDialog):
+    def __init__(self):
+        super(LoginPage, self).__init__()
+        loadUi('UI/login_page.ui', self)
+        self.password_field.setEchoMode(QtWidgets.QLineEdit.Password)
+        self.login.clicked.connect(self.login_function)
+
+    def login_function(self):
+        user = self.username_field.text()
+        password = self.password_field.text()
+
+        if len(user) == 0 or len(password) == 0:
+            self.error.setText('Please input all fields!')
+        elif ' ' in user or ' ' in password:
+            self.error.setText('Write correct password and username!')
+        else:
+            send_message('LOGIN', f'{user} {password}')
+
+
+class RegistrationPage(QDialog):
+    def __init__(self):
+        super(RegistrationPage, self).__init__()
+        loadUi('UI/signup_page.ui', self)
+        self.password_field.setEchoMode(QtWidgets.QLineEdit.Password)
+        self.password_confirm_field.setEchoMode(QtWidgets.QLineEdit.Password)
+        self.signup.clicked.connect(self.signup_function)
+
+    def signup_function(self):
+        global msg
+        global head
+        user = self.username_field.text()
+        password = self.password_field.text()
+        password_confirm = self.password_confirm_field.text()
+
+        if len(user) == 0 or len(password) == 0 or len(password_confirm) == 0:
+            self.error.setText('Please input all fields!')
+        elif password != password_confirm:
+            self.error.setText('You wrote two different passwords!')
+        else:
+            send_message('SIGNUP', f'{user} {password}')
+
+
+class MenuPage(QDialog):
+    def __init__(self):
+        super(MenuPage, self).__init__()
+        loadUi('UI/menu.ui')
+
+
+if __name__ == '__main__':
+    gniazdo = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    gniazdo.connect(("localhost", 8021))
+
+    def send_message(header, content):
+        """
+        :example:
+        send_message("SIGNUP", "user password")
+        send_message("LOGIN", "user password")
+        """
+        message = f'\t{header}\t{content}\t'
+        for i in message:
+            gniazdo.send(i.encode('ascii'))
+
+    NO_HEADER_NO_MESSAGE = 1
+    HEADER = 2
+    MESSAGE = 3
+    GLOBAL_STATE = NO_HEADER_NO_MESSAGE
+    GLOBAL_HEADER = ''
+    GLOBAL_MSG = ''
+
+    def interpret_message():
+        global GLOBAL_HEADER
+        global GLOBAL_MSG
+        print("Odczytaned: ", GLOBAL_HEADER, GLOBAL_MSG)
+        if GLOBAL_HEADER == 'SIGNUP_R SUCCESS':
+            current_widget = widget.currentWidget()
+            widget.setCurrentIndex(widget.currentIndex() - 1)
+            widget.removeWidget(current_widget)
+        elif GLOBAL_HEADER == 'LOGIN_R SUCCESS':
+            menu = MenuPage()
+            widget.addWidget(menu)
+            widget.setCurrentIndex(widget.currentIndex() + 1)
+
+    def parser(znak):
+        global GLOBAL_STATE
+        global GLOBAL_MSG
+        global GLOBAL_HEADER
+
+        if GLOBAL_STATE == NO_HEADER_NO_MESSAGE:
+            if znak == '\t':
+                GLOBAL_STATE = HEADER
+                return
+
+        elif GLOBAL_STATE == HEADER:
+            if znak == '\t':
+                GLOBAL_STATE = MESSAGE
+                return
+            GLOBAL_HEADER += znak
+
+        elif GLOBAL_STATE == MESSAGE:
+            if znak == '\t':
+                interpret_message()
+                GLOBAL_STATE = NO_HEADER_NO_MESSAGE
+                GLOBAL_MSG = ""
+                GLOBAL_HEADER = ""
+            GLOBAL_MSG += znak
+
+
+    class Receiver(Thread):
+        def __init__(self):
+            Thread.__init__(self)
+            self.daemon = True
+            self.start()
+
+        def run(self):
+            while True:
+                buf = gniazdo.recv(1)
+                if len(str(buf)) == 0:
+                    continue
+                parser(buf.decode('ascii')[0])
+
+
+    Receiver()
+    app = QApplication(sys.argv)
+    welcome = WelcomePage()
+    widget = QtWidgets.QStackedWidget()
+    widget.addWidget(welcome)
+    widget.setFixedHeight(800)
+    widget.setFixedWidth(1200)
+    widget.show()
+    try:
+        sys.exit(app.exec_())
+    except:
+        print("Exiting")
