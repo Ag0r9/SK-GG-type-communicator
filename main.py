a1e7743d82697559ef8604cb0ebda0788daa7d75
Ag0r9 <51130332+Ag0r9@users.noreply.github.com>
2022-01-12 01:17:32 +0100

    (dirty) need to fix friend remove

--- main.py
+++ main.py
@@ -4,7 +4,9 @@ from threading import Thread
 
 from PyQt5.uic import loadUi
 from PyQt5 import QtWidgets
-from PyQt5.QtWidgets import QDialog, QApplication
+from PyQt5.QtWidgets import QDialog, QApplication, QMessageBox
+
+from utils import string_to_list
 
 
 class WelcomePage(QDialog):
@@ -57,6 +59,8 @@ class RegistrationPage(QDialog):
             self.error.setText('Please input all fields!')
         elif password != password_confirm:
             self.error.setText('You wrote two different passwords!')
+        elif ' ' in user or ' ' in password:
+            self.error.setText('Write correct password and username!')
         else:
             send_message('SIGNUP', f'{user} {password}')
 
@@ -66,10 +70,39 @@ class MenuPage(QDialog):
         super(MenuPage, self).__init__()
         loadUi('UI/menu_page.ui', self)
 
+        self.friend_id = None
+        self.friendlist.currentItemChanged.connect(self.friend_clicked)
+        self.add_friend_button.clicked.connect(self.add_friend_function)
+        self.remove_friend_button.clicked.connect(self.remove_friend_function)
+        self.send_button.clicked.connect(self.send_function)
+
+    def friend_clicked(self):
+        self.friend_id = self.friendlist.currentItem().text().split()[-1]
+
+    def send_function(self):
+        if self.friend_id is not None:
+            text = f'{self.friend_id} {self.chat_line.text()}'
+            if self.chat_line.text():
+                send_message('SEND_MESSAGE', text)
+
+    def remove_friend_function(self):
+        if self.friend_id is not None:
+            send_message('REMOVE_FRIEND', self.friend_id)
+            send_message('FRIENDS_LIST', '')
+
+    def add_friend_function(self):
+        friend = self.nick_input.text()
+        if ' ' in friend or friend == '':
+            self.error.setText('Write correct username!')
+        else:
+            send_message('ADD_FRIEND', friend)
+            send_message('FRIENDS_LIST', '')
+
 
 if __name__ == '__main__':
     gniazdo = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     gniazdo.connect(("localhost", 8021))
+
 
     def send_message(header, content):
         """
@@ -81,6 +114,7 @@ if __name__ == '__main__':
         for i in message:
             gniazdo.send(i.encode('ascii'))
 
+
     NO_HEADER_NO_MESSAGE = 1
     HEADER = 2
     MESSAGE = 3
@@ -88,14 +122,28 @@ if __name__ == '__main__':
     GLOBAL_HEADER = ''
     GLOBAL_MSG = ''
 
+
     def interpret_message():
         global GLOBAL_HEADER
         global GLOBAL_MSG
         print("Odczytaned:", GLOBAL_HEADER, GLOBAL_MSG)
         if GLOBAL_HEADER == 'SIGNUP_R SUCCESS':
             widget.setCurrentIndex(0)
+        if GLOBAL_HEADER == 'SIGNUP_R FAILED' and GLOBAL_MSG == 'USERNAME_TAKEN':
+            msg.setText('Username taken!')
+        if GLOBAL_HEADER == 'SIGNUP_R SUCCESS':
+            widget.setCurrentIndex(0)
         elif GLOBAL_HEADER == 'LOGIN_R SUCCESS':
+            send_message('FRIENDS_LIST', '')
             widget.setCurrentIndex(3)
+        elif GLOBAL_HEADER == 'ADD_FRIEND_R SUCCESS':
+            msg.setText('Friend added!')
+        elif GLOBAL_HEADER == 'FRIENDS_LIST_R SUCCESS':
+            friend_list = string_to_list(GLOBAL_MSG)
+            menu_page.friendlist.clear()
+            for i in friend_list:
+                menu_page.friendlist.addItem(f'{i[2]}->{i[1]} {i[0]}')
+
 
     def parser(znak):
         global GLOBAL_STATE
@@ -122,6 +170,10 @@ if __name__ == '__main__':
             GLOBAL_MSG += znak
 
 
+    def my_exit_handler():
+        send_message('LOGOUT', '')
+
+
     class Receiver(Thread):
         def __init__(self):
             Thread.__init__(self)
@@ -138,11 +190,17 @@ if __name__ == '__main__':
 
     Receiver()
     app = QApplication(sys.argv)
+    app.aboutToQuit.connect(my_exit_handler)
+    msg = QMessageBox()
     widget = QtWidgets.QStackedWidget()
-    widget.addWidget(WelcomePage())  # 0
-    widget.addWidget(LoginPage())  # 1
-    widget.addWidget(RegistrationPage())  # 2
-    widget.addWidget(MenuPage())  # 3
+    welcome_page = WelcomePage()
+    widget.addWidget(welcome_page)  # 0
+    login_page = LoginPage()
+    widget.addWidget(login_page)  # 1
+    registration_page = RegistrationPage()
+    widget.addWidget(registration_page)  # 2
+    menu_page = MenuPage()
+    widget.addWidget(menu_page)  # 3
     widget.setFixedHeight(800)
     widget.setFixedWidth(1200)
     widget.show()
--- utils.py
+++ utils.py
@@ -1,6 +1,13 @@
 def string_to_list(string):
     string = string.strip()
+    if not string:
+        return []
     string = string[1:-1]
     friends = string.split('] [')
     friends = [friend.split(' ') for friend in friends]
+    for idx in range(len(friends)):
+        if int(friends[idx][1]):
+            friends[idx][1] = 'active'
+        else:
+            friends[idx][1] = 'inactive'
     return friends
